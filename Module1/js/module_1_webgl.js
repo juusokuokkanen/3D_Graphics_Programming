d2cbba8069b09e62d50d01d05d85189a1beac26b
Juuso Kuokkanen <juuso.kuokkanen@gmail.com>
2015-01-18 20:01:51 +0200

    Added code that gets webgl context and empty scene

--- Module1/js/module_1_webgl.js
+++ Module1/js/module_1_webgl.js
@@ -5,3 +5,25 @@
  */
 
 
+window.onload = initWebGL();
+var gl = null;
+
+function initWebGL(){
+    try{
+        //get the canvas element with its id
+        var canvas = document.getElementById("canvas-webgl");
+        //get webgl context to global gl variable
+        gl = canvas.getContext("webgl");
+    }
+    catch(e){
+        
+    }
+    //check that context was gotten and fill the color buffer
+    if(gl){
+        //we can access the webgl functionalities by usin gl, that contains the context
+        gl.clearColor(1.0, 0.0, 0.0, 1.0);
+        gl.clear(gl.COLOR_BUFFER_BIT);
+    }else{
+        alert("Error getting the context. Browser may not support WebGL");
+    }
+}
