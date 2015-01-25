0a5eeae34957a7b58e25b9db942575adcf1f0c25
Juuso Kuokkanen <juuso.kuokkanen@gmail.com>
2015-01-25 19:21:15 +0200

    Added code for texturing the quad, but needs still some tinkering

--- Module1/js/module_1_threejs.js
+++ Module1/js/module_1_threejs.js
@@ -7,19 +7,34 @@
 var renderer = null;
 var quad = null;
 var angle = 0;
+var renderer = null;
+var scene = null;
+var camera = null;
+var mesh = null;
+var texture = THREE.ImageUtils.loadTexture("js/kitty.jpg");;
 
+var meshOriginPositionX;
 window.onload = function(){
     startThree();
 };
 
+function animLoop(){
+        //rotate mesh around y-axis
+        mesh.position.x = meshOriginPositionX + 2.0*Math.cos(angle);
+        mesh.rotation.y = angle;
+        angle += 0.05;
+        requestAnimationFrame(animLoop);
+        renderer.render(scene, camera);
+};
+
 function startThree(){
     //Setup renderer and atach it to document
-    var renderer = new THREE.WebGLRenderer();
+    renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
     
     //Create perpective camera and move it back along z axis
-    var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 10000);
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 10000);
     camera.position.set(0, 0, 10);
     
     
@@ -29,7 +44,7 @@ function startThree(){
     clearColor.setRGB(0.0, 0.0, 1.0);
     
     //Create empty scene
-    var scene = new THREE.Scene();
+    scene = new THREE.Scene();
 
     //Create quad geometry by givin each vertex position, and defining faces/indices
     quad = new THREE.Geometry();
@@ -52,21 +67,36 @@ function startThree(){
     quad.faces[1].vertexColors.push(new THREE.Color(0xFFFF00));
     quad.faces[1].vertexColors.push(new THREE.Color(0xFF0000));
     quad.faces[1].vertexColors.push(new THREE.Color(0x00FF00));
+    
+    quad.faceVertexUvs.push(new THREE.Vector2(0, 0));
+    quad.faceVertexUvs.push(new THREE.Vector2(1, 0));
+    quad.faceVertexUvs.push(new THREE.Vector2(0, 0));
+    quad.faceVertexUvs.push(new THREE.Vector2(1, 1));
+    //load texture
+    //texture = THREE.ImageUtils.loadTexture("js/kitty.jpg");
+    texture.wrapT = texture.wrapS = THREE.RepeatWrapping;
+    //texture.repeat.set( 4, 4 );
+    texture.needsUpdate = true;
 
     //set quad material with quadColor (green)
     var material = new THREE.MeshBasicMaterial({
         //color: quadColor
-        vertexColors: THREE.VertexColors
+        //vertexColors: THREE.VertexColors
+        map: texture
     });
     
     //create mesh from quad geometry and material
-    var mesh = new THREE.Mesh(quad, material);
-    
+    mesh = new THREE.Mesh(quad, material);
+    mesh.doubleSided = true;
     //Add quad mesh to scene
     scene.add(mesh);
     
     //set renderer clear color to blue and render with scene and camera
     renderer.setClearColor(clearColor, 1.0);
-    renderer.render(scene, camera);
+    
+    meshOriginPositionX = mesh.position.x;
+    
+    animLoop();
+    
 }
 
